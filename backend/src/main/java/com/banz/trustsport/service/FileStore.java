fa81b37e10c18cdfc5bc818f18b23bf8b247cccb
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2024-03-19 12:33:49 +0200

    refactored download and added upload image to aws so the feature is more flexible

--- backend/src/main/java/com/banz/trustsport/service/FileStore.java
+++ backend/src/main/java/com/banz/trustsport/service/FileStore.java
@@ -6,14 +6,20 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.util.IOUtils;
-import jakarta.validation.constraints.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.banz.trustsport.enums.Bucket;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 
 @Service
@@ -21,7 +27,6 @@ public class FileStore {
 
     private final AmazonS3 s3;
 
-    @Autowired
     public FileStore(AmazonS3 s3) {
         this.s3 = s3;
     }
@@ -46,11 +51,35 @@ public class FileStore {
 
     public byte[] download(String path, String thumbnailLink) {
         try {
-            S3Object object = s3.getObject(path,thumbnailLink);
-            S3ObjectInputStream inputStream = object.getObjectContent();
-            return IOUtils.toByteArray(inputStream);
-        } catch (AmazonServiceException |IOException e) {
-            throw new RuntimeException("Failed to download", e);
+            S3Object object = s3.getObject(path, thumbnailLink);
+            try (S3ObjectInputStream inputStream = object.getObjectContent()) {
+                return IOUtils.toByteArray(inputStream);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to read S3 object content", e);
+            }
+        } catch (AmazonServiceException e) {
+            // Log or handle the AmazonServiceException appropriately
+            throw new RuntimeException("Failed to download from S3", e);
         }
+    }
+
+    public String uploadImage(MultipartFile file) {
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", "image/webp"); // Set content type to WebP
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+
+        String path = String.format("%s", Bucket.IMAGES.getBucketName()); // Customize path based on user ID
+        String filename = String.format("%s.webp", UUID.randomUUID()); // Generate unique filename with .webp extension
+
+        try {
+            BufferedImage image = ImageIO.read(file.getInputStream());
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ImageIO.write(image,"webp", outputStream);
+            // Save the uploaded image to the file store
+            save(path, filename, Optional.of(metadata), new ByteArrayInputStream(outputStream.toByteArray()));
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to upload image", e);
+        }
+        return filename;
     }
 }
