7809b58993a0ee351d5d005e4565dc9d76c806f5
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2024-03-19 12:39:08 +0200

    created PostNotFoundException.javat

---
+++ backend/src/main/java/com/banz/trustsport/exceptions/PostNotFoundException.java
@@ -0,0 +1,10 @@
+package com.banz.trustsport.exceptions;
+
+public class PostNotFoundException extends RuntimeException {
+
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+
+}
+
