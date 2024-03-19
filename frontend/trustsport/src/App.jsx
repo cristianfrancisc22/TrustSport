29433bf2d6c5ec510023c5be991df5501c59650f
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2024-03-19 13:29:48 +0200

    refactored existing routes and added new routes

--- frontend/trustsport/src/App.jsx
+++ frontend/trustsport/src/App.jsx
@@ -1,37 +1,44 @@
 import React from 'react';
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import RootLayout from './layouts/RootLayout';
 import SignupPage from './pages/Signup';
+import Home from "./components/Home";
 import LoginPage from './pages/Login';
 import Main from './pages/Main';
 import TeamNews from './pages/TeamNews';
 import NewsArticle from './pages/NewsArticle';
 import ChampionshipNews from './pages/ChampionshipNews';
 import CreatePost from './pages/CreatePost';
+import SubscriptionPlanPage from './pages/SubscriptionPlanPage';
+import Checkout from './components/Checkout';
+import Success from "./components/Success";
+import Failure from "./components/Failure";
 
 export default function App() {
   return (
-    <div>
-      <Header />
-      <div className=" p-10 mb-10"> {/* Adjust the top margin to accommodate the header */}
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/teamNews/:team" element={<TeamNews />} />
-            <Route path="/news/:id" element={<NewsArticle />} />
-            <Route path="/:championship" element={<ChampionshipNews />} />
-            <Route path="/createPost" element={<CreatePost />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-      <Footer />
-    </div>
+    <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Main />} />
+        <Route path="checkout/:price/:duration/:priceId" element={<Checkout />} />
+        <Route path="home" element={<Home />} />
+        <Route path="post/:team" element={<TeamNews />} />
+        <Route path="news/:id" element={<NewsArticle />} />
+        <Route path="post/create" element={<CreatePost />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="new-subscription" element={<SubscriptionPlanPage />} />
+        <Route path='failure' element={<Failure />} />
+        <Route path='success' element={<Success />} />
+        <Route path="championship/:championship" element={<ChampionshipNews />} />
+        
+      </Route>
+    </Routes>
+    <Footer />
+  </Router>
+  
   );
 }
