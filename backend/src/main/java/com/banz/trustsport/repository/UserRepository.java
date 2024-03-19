9777b8fc3e559f46f0cd78b13cb9c7211d3fa5df
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2024-03-19 13:01:57 +0200

    added update description query

--- backend/src/main/java/com/banz/trustsport/repository/UserRepository.java
+++ backend/src/main/java/com/banz/trustsport/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.banz.trustsport.repository;
 
 import com.banz.trustsport.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,12 +13,12 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-    User findAppUserByUsername(String username);
 
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
 
-    User findAppUserById(Long id);
-
+    @Modifying
+    @Query("UPDATE User u SET u.description = :description WHERE u.id = :userId")
+    void updateDescription(@Param("userId") Long userId, @Param("description") String description);
 }
