6cedfb068fe95baa350cc13945b5b41364e75f72
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2024-03-19 14:27:23 +0200

    subscriber plans

---
+++ frontend/trustsport/src/data/Plans.js
@@ -0,0 +1,30 @@
+export const Plans = [
+    {
+        id: 1,
+        duration: 1,
+        description: "Primesti mail-uri zilnice timp de 1 luna de zile cu cele mai importante știri despre lucrurile care te interesează.",
+        price: 15,
+        priceId: "price_1OrIfDBHVJf9n41ChxqJIM0w"
+    },
+    {
+        id: 2,
+        duration: 3,
+        description: "Primesti mail-uri zilnice timp de 3 luni de zile cu cele mai importante știri despre lucrurile care te interesează.",
+        price: 36,
+        priceId: "price_1OrIgvBHVJf9n41Cq76XWNr4"
+    },
+    {
+        id: 3,
+        duration: 6,
+        description: "Primesti mail-uri zilnice timp de 6 luni de zile cu cele mai importante știri despre lucrurile care te interesează.",
+        price: 60,
+        priceId: "price_1OrIhBBHVJf9n41CATPR2LGd"
+    },
+    {
+        id: 4,
+        duration: 12,
+        description: "Primesti mail-uri zilnice timp de 1 an de zile cu cele mai importante știri despre lucrurile care te interesează.",
+        price: 100,
+        priceId: "price_1OrIhNBHVJf9n41CqYFZBQo3"
+    },
+]
