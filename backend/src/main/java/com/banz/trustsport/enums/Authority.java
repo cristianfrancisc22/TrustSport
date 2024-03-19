c22ff10a8a6f156ace8cb84bda5af68c667bcfcf
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2024-03-19 12:32:04 +0200

    added more authorities and converting string to ENUM function

--- backend/src/main/java/com/banz/trustsport/enums/Authority.java
+++ backend/src/main/java/com/banz/trustsport/enums/Authority.java
@@ -1,8 +1,28 @@
 package com.banz.trustsport.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Authority {
-    USER,
-    ADMIN,
-    JOURNALIST
+    ADMIN("Admin"),
+    USER("User"),
+    JOURNALIST("Journalist"),
+    MODERATOR("Moderator");
+
+    private final String displayName;
+
+    Authority(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public static Authority fromString(String roleString) {
+        for (Authority role : Authority.values()) {
+            if (role.displayName.equalsIgnoreCase(roleString.trim())) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("Invalid role: " + roleString);
+    }
 }
+
 
