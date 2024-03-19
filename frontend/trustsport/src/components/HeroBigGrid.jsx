50f1ffdcfe70280b62a33d449503111d1f4418ed
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2024-03-19 14:21:23 +0200

    added paths for posts

--- frontend/trustsport/src/components/HeroBigGrid.jsx
+++ frontend/trustsport/src/components/HeroBigGrid.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import defaultThumbnail from '../img/ads/default.webp'; // Import the default thumbnail image
+import { Grid } from '@chakra-ui/react';
 
 // HeroBigGrid component
 const HeroBigGrid = ({ news }) => {
   return (
-    <div className="bg-white py-6">
+    <Grid pt='6'>
       <div className="container mx-auto px-3 sm:px-4 xl:px-2">
         {/* big grid 1 */}
         <div className="flex flex-row flex-wrap">
@@ -14,56 +16,50 @@ const HeroBigGrid = ({ news }) => {
           <BoxNews news={news.slice(1)} />
         </div>
       </div>
-    </div>
+    </Grid>
   );
 };
 
 const LeftCover = ({ news }) => {
-  const { title, text, sport, championship, id } = news;
-  const [imageLoaded, setImageLoaded] = useState(false);
+  const { title, text, sportType, championship, id } = news;
+  const [imageLoaded, setImageLoaded] = useState(true); // Initialize to true
 
   useEffect(() => {
-    setImageLoaded(false); // Reset imageLoaded when the news prop changes
+    setImageLoaded(true); // Reset imageLoaded when the news prop changes
   }, [news]);
 
-  // Function to truncate text to 10-15 words
-  const truncateText = (text) => {
-    const words = text.split(' ');
-    if (words.length <= 15) {
-      return text;
-    } else {
-      return words.slice(0, 15).join(' ') + '...';
-    }
-  };
 
-  const truncatedText = truncateText(text);
-
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  }
+  
   return (
-    <div className="flex-shrink w-full lg:w-1/2 pb-1 lg:pb-0 lg:pr-1">
+    // <Card>
+
+    // </Card>
+    <div className="flex-shrink w-full lg:w-1/2 pb-1 lg:pb-0 lg:pr-1 hover:border-red-500 border-8 border-black border-double">
+      <Link to={{ pathname: `/news/${id}`, state: news}} >
       <div className="relative hover-img max-h-98 overflow-hidden">
-        <a href="#">
           <img
             className="max-w-full w-full mx-auto h-auto"
-            src={imageLoaded ? `http://localhost:8080/api/v1/downloadThumbnail/${id}` : defaultThumbnail}
+            src={`http://localhost:8080/api/v1/post/thumbnail/download/${id}?${imageLoaded ? 'loaded=true' : ''}`}
             alt="Image description"
-            onLoad={() => setImageLoaded(true)}
+            onLoad={handleImageLoad}
           />
-        </a>
         <div className="absolute px-5 pt-8 pb-5 bottom-0 w-full bg-gradient-cover">
-          <a href="#">
             <h2 className="text-3xl font-bold capitalize text-white mb-3">{title}</h2>
-          </a>
           <div className="text-gray-100 hidden sm:inline-block" dangerouslySetInnerHTML={{ __html: truncatedText }}></div> {/* Render truncated HTML content */}
           <div className="pt-2">
             <div className="text-gray-100 flex items-center">
               <div className="inline-block h-3 border-l-2 border-red-600 mr-2"></div>
-              <span>{sport}</span>
+              <span>{sportType}</span>
               <div className="inline-block custom-dot"></div>
               <span>{championship}</span>
             </div>
           </div>
         </div>
       </div>
+      </Link>
     </div>
   );
 };
@@ -83,38 +79,40 @@ const BoxNews = ({ news }) => {
 
 // NewsArticle component
 const NewsArticle = ({ news }) => {
-  const { title, sport, championship, id } = news;
-  const [imageLoaded, setImageLoaded] = useState(false);
+  const { title, sportType, championship, id } = news;
+  const [imageLoaded, setImageLoaded] = useState(true); // Initialize to true
 
   useEffect(() => {
-    setImageLoaded(false); // Reset imageLoaded when the news prop changes
+    setImageLoaded(true); // Reset imageLoaded when the news prop changes
   }, [news]);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
 
   return (
     <article className="flex-shrink w-full sm:w-1/2">
+      <Link to={{ pathname: `/news/${id}`, state: news}} >
       <div className="relative hover-img max-h-48 overflow-hidden">
-        <a href="#">
           <img
             className="max-w-full w-full mx-auto h-auto"
-            src={imageLoaded ? `http://localhost:8080/api/v1/downloadThumbnail/${id}` : defaultThumbnail}
+            src={`http://localhost:8080/api/v1/post/thumbnail/download/${id}?${imageLoaded ? 'loaded=true' : ''}`}
             alt="Image description"
-            onLoad={() => setImageLoaded(true)}
+            onLoad={handleImageLoad}
           />
-        </a>
         <div className="absolute px-4 pt-7 pb-4 bottom-0 w-full bg-gradient-cover">
-          <a href="#">
             <h2 className="text-lg font-bold capitalize leading-tight text-white mb-1">{title}</h2>
-          </a>
           <div className="pt-1">
             <div className="text-gray-100 flex items-center">
               <div className="inline-block h-3 border-l-2 border-red-600 mr-2"></div>
-              <span>{sport}</span>
+              <span>{sportType}</span>
               <div className="inline-block custom-dot"></div>
               <span>{championship}</span>
             </div>
           </div>
         </div>
       </div>
+      </Link>
     </article>
   );
 };
