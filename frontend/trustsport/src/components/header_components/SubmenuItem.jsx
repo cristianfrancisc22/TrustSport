ef3c72da6e5e3e9df6149276122c284cf5b50bdc
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2024-03-19 14:27:09 +0200

    added link for each championship in navbard

--- frontend/trustsport/src/components/header_components/SubmenuItem.jsx
+++ frontend/trustsport/src/components/header_components/SubmenuItem.jsx
@@ -1,10 +1,14 @@
 // SubmenuItem.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function SubmenuItem({ href, text }) {
+
   return (
     <li className="relative hover:bg-gray-50 ">
-      <a className="block py-2 px-6 border-b border-gray-100 " href={href}>{text}</a>
+      <Link to={{ pathname: `/championship/${href}`, state: text}} className="block py-2 px-6 border-b border-gray-100 ">
+        {text}
+      </Link>
     </li>
   );
 }
