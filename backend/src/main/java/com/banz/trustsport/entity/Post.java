66476b56dc94a6aee4a3c57e6f517a3218ccd1dd
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2024-03-19 12:29:32 +0200

    added builder pattern and extending auditable now

--- backend/src/main/java/com/banz/trustsport/entity/Post.java
+++ backend/src/main/java/com/banz/trustsport/entity/Post.java
@@ -1,75 +1,119 @@
 package com.banz.trustsport.entity;
 
-
 import com.banz.trustsport.enums.PostType;
+import com.banz.trustsport.enums.SportType;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
+import lombok.*;
 
-
-import java.awt.*;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Setter
 @Entity
-public class Post {
+public class Post extends Auditable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @CreationTimestamp
-    private ZonedDateTime date;
-
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
-
-    @NotNull
     private String title;
-
-    @Column(columnDefinition = "TEXT")
     private String text;
-
-    @NotNull
-    @Column(length = 50)
-    private String sport;
-
-    //FRONTEND -> TRANSFERMARKT API -> BACKEND
+    @Enumerated(EnumType.STRING)
+    private SportType sportType;
     private String championship;
-
     private String team;
-
-    private String player;
-
-    @Column(name = "national_news")
-    private boolean nationalNews;
-
+    private boolean nationalPost;
     @Enumerated(EnumType.STRING)
     private PostType postType;
+    private String thumbnail;
+    private Long views = 0L;
 
-    private String thumbnailLink;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ImageLink> images = new ArrayList<>();
-
-    @Column(name = "view_count")
-    private int viewCount = 0;
-
-    public void incrementViewCount() {
-        this.viewCount++;
+    public Post(Long id, String title, String text, SportType sportType, String championship, String team, String thumbnail) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+        this.sportType = sportType;
+        this.championship = championship;
+        this.team = team;
+        this.thumbnail = thumbnail;
     }
 
+    public Post(Long id, String title, String thumbnail, String createdBy) {
+        this.id = id;
+        this.title = title;
+        this.thumbnail = thumbnail;
+        setCreatedBy(createdBy);
+    }
 
-    // Additional properties or methods common to all posts
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private final Post post = new Post();
+
+        public Builder id(Long id) {
+            post.id = id;
+            return this;
+        }
+
+        public Builder user(User user) {
+            post.user = user;
+            return this;
+        }
+
+        public Builder title(String title) {
+            post.title = title;
+            return this;
+        }
+
+        public Builder text(String text) {
+            post.text = text;
+            return this;
+        }
+
+        public Builder sportType(SportType sportType) {
+            post.sportType = sportType;
+            return this;
+        }
+
+        public Builder championship(String championship) {
+            post.championship = championship;
+            return this;
+        }
+
+        public Builder team(String team) {
+            post.team = team;
+            return this;
+        }
+
+        public Builder nationalPost(boolean nationalPost) {
+            post.nationalPost = nationalPost;
+            return this;
+        }
+
+        public Builder postType(PostType postType) {
+            post.postType = postType;
+            return this;
+        }
+
+        public Builder thumbnail(String thumbnail) {
+            post.thumbnail = thumbnail;
+            return this;
+        }
+
+        public Builder views(Long views) {
+            post.views = views;
+            return this;
+        }
+
+        public Post build() {
+            // Add validation logic here if needed
+            return post;
+        }
+
+    }
 }
