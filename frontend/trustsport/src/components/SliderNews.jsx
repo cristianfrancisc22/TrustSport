7715f06e1f3e0ef874e09a4eaf69a68548942f3d
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2024-03-19 14:23:33 +0200

    refactored class into className

--- frontend/trustsport/src/components/SliderNews.jsx
+++ frontend/trustsport/src/components/SliderNews.jsx
@@ -1,102 +1,102 @@
-<div class="relative bg-gray-50" >
-                <div class="bg-black bg-opacity-70">
-                    <div class="xl:container mx-auto px-3 sm:px-4 xl:px-2">
-                    <div class="flex flex-row flex-wrap">
-                        <div class="flex-shrink max-w-full w-full py-12 overflow-hidden">
-                        <div class="w-full py-3">
-                            <h2 class="text-white text-2xl font-bold text-shadow-black">
-                            <span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>American
+<div className="relative bg-gray-50" >
+                <div className="bg-black bg-opacity-70">
+                    <div className="xl:container mx-auto px-3 sm:px-4 xl:px-2">
+                    <div className="flex flex-row flex-wrap">
+                        <div className="flex-shrink max-w-full w-full py-12 overflow-hidden">
+                        <div className="w-full py-3">
+                            <h2 className="text-white text-2xl font-bold text-shadow-black">
+                            <span className="inline-block h-5 border-l-3 border-red-600 mr-2"></span>American
                             </h2>
                         </div>
-                        <div id="post-carousel" class="splide post-carousel">
-                            <div class="splide__track">
-                            <ul class="splide__list">
-                                <li class="splide__slide">
-                                <div class="w-full pb-3">
-                                    <div class="hover-img bg-white">
+                        <div id="post-carousel" className="splide post-carousel">
+                            <div className="splide__track">
+                            <ul className="splide__list">
+                                <li className="splide__slide">
+                                <div className="w-full pb-3">
+                                    <div className="hover-img bg-white">
                                     <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="src/img/dummy/img14.jpg" alt="alt title"/>
+                                        <img className="max-w-full w-full mx-auto" src="src/img/dummy/img14.jpg" alt="alt title"/>
                                     </a>
-                                    <div class="py-3 px-6">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
+                                    <div className="py-3 px-6">
+                                        <h3 className="text-lg font-bold leading-tight mb-2">
                                         <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
                                         </h3>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Europe</a>
+                                        <a className="text-gray-500" href="#"><span className="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Europe</a>
                                     </div>
                                     </div>
                                 </div>
                                 </li>
-                                <li class="splide__slide">
-                                <div class="w-full pb-3">
-                                    <div class="hover-img bg-white">
+                                <li className="splide__slide">
+                                <div className="w-full pb-3">
+                                    <div className="hover-img bg-white">
                                     <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="src/img/dummy/img15.jpg" alt="alt title"/>
+                                        <img className="max-w-full w-full mx-auto" src="src/img/dummy/img15.jpg" alt="alt title"/>
                                     </a>
-                                    <div class="py-3 px-6">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
+                                    <div className="py-3 px-6">
+                                        <h3 className="text-lg font-bold leading-tight mb-2">
                                         <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
                                         </h3>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Europe</a>
+                                        <a className="text-gray-500" href="#"><span className="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Europe</a>
                                     </div>
                                     </div>
                                 </div>
                                 </li>
-                                <li class="splide__slide">
-                                <div class="w-full pb-3">
-                                    <div class="hover-img bg-white">
+                                <li className="splide__slide">
+                                <div className="w-full pb-3">
+                                    <div className="hover-img bg-white">
                                     <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="src/img/dummy/img16.jpg" alt="alt title"/>
+                                        <img className="max-w-full w-full mx-auto" src="src/img/dummy/img16.jpg" alt="alt title"/>
                                     </a>
-                                    <div class="py-3 px-6">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
+                                    <div className="py-3 px-6">
+                                        <h3 className="text-lg font-bold leading-tight mb-2">
                                         <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
                                         </h3>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Europe</a>
+                                        <a className="text-gray-500" href="#"><span className="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Europe</a>
                                     </div>
                                     </div>
                                 </div>
                                 </li>
-                                <li class="splide__slide">
-                                <div class="w-full pb-3">
-                                    <div class="hover-img bg-white">
+                                <li className="splide__slide">
+                                <div className="w-full pb-3">
+                                    <div className="hover-img bg-white">
                                     <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="src/img/dummy/img17.jpg" alt="alt title"/>
+                                        <img className="max-w-full w-full mx-auto" src="src/img/dummy/img17.jpg" alt="alt title"/>
                                     </a>
-                                    <div class="py-3 px-6">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
+                                    <div className="py-3 px-6">
+                                        <h3 className="text-lg font-bold leading-tight mb-2">
                                         <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
                                         </h3>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Europe</a>
+                                        <a className="text-gray-500" href="#"><span className="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Europe</a>
                                     </div>
                                     </div>
                                 </div>
                                 </li>
-                                <li class="splide__slide">
-                                <div class="w-full pb-3">
-                                    <div class="hover-img bg-white">
+                                <li className="splide__slide">
+                                <div className="w-full pb-3">
+                                    <div className="hover-img bg-white">
                                     <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="src/img/dummy/img18.jpg" alt="alt title"/>
+                                        <img className="max-w-full w-full mx-auto" src="src/img/dummy/img18.jpg" alt="alt title"/>
                                     </a>
-                                    <div class="py-3 px-6">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
+                                    <div className="py-3 px-6">
+                                        <h3 className="text-lg font-bold leading-tight mb-2">
                                         <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
                                         </h3>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Europe</a>
+                                        <a className="text-gray-500" href="#"><span className="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Europe</a>
                                     </div>
                                     </div>
                                 </div>
                                 </li>
-                                <li class="splide__slide">
-                                <div class="w-full pb-3">
-                                    <div class="hover-img bg-white">
+                                <li className="splide__slide">
+                                <div className="w-full pb-3">
+                                    <div className="hover-img bg-white">
                                     <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="src/img/dummy/img1.jpg" alt="alt title"/>
+                                        <img className="max-w-full w-full mx-auto" src="src/img/dummy/img1.jpg" alt="alt title"/>
                                     </a>
-                                    <div class="py-3 px-6">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
+                                    <div className="py-3 px-6">
+                                        <h3 className="text-lg font-bold leading-tight mb-2">
                                         <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
                                         </h3>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Europe</a>
+                                        <a className="text-gray-500" href="#"><span className="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Europe</a>
                                     </div>
                                     </div>
                                 </div>
