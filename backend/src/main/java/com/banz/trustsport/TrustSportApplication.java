52f585df666eacdbb63c2e41eaa322743dc2b7ef
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2024-03-19 12:41:46 +0200

    enabling jpa auditing

--- backend/src/main/java/com/banz/trustsport/TrustSportApplication.java
+++ backend/src/main/java/com/banz/trustsport/TrustSportApplication.java
@@ -1,33 +1,19 @@
 package com.banz.trustsport;
 
 import com.banz.trustsport.config.RsaKeyProperties;
-import com.banz.trustsport.entity.User;
-import com.banz.trustsport.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class TrustSportApplication {
-	@Autowired
-	UserRepository userRepository;
-	@Autowired
-	PasswordEncoder encoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrustSportApplication.class, args);
 	}
 
 
-//	@Bean
-//	CommandLineRunner commandLineRunner(){
-//		return args -> {
-//			userRepository.save(new User("cristi","cristian.francisc22@gmail.com",encoder.encode("parolacodata")));
-//		};
-//	}
 }
